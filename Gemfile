0e567f0e09b0eeadec65ac5b9c182e77aa908d06
Lisa Argueta <devcub22@gmail.com>
2016-05-13 05:40:26 +0000

    set up the maps integration

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem 'sprockets', '2.11.0'
 gem 'sass-rails'
 gem 'simple_form'
 gem 'devise', '>= 3.2.4'
+gem 'geocoder'
+gem 'figaro', '>= 1.0.0'
 
 # Gemfile for Rails 3+, Sinatra, and Merb
 gem 'will_paginate', '~> 3.1.0'
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
+
   validates_presence_of :name, :address, :description
   validates_length_of :name, minimum: 4
 end
