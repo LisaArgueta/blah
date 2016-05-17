1a58b6237ef40ed6b63cddbc24c06c7cb5898a39
Lisa Argueta <devcub22@gmail.com>
2016-05-17 22:37:20 +0000

    commit

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -25,7 +25,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-    process :scale => [50, 50]
+    #process :scale => [50, 50]
   #
   # def scale(width, height)
   #   # do something
